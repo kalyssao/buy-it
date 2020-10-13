0bf9bca772d5d58d8deaf5478bd4cd74a5772c98
kalyssao <klysssa@icloud.com>
2020-10-13 22:04:20 +0000

    making stuff modular

---
+++ flask-app/Decision.py
@@ -0,0 +1,5 @@
+class Decision:
+    def sentiment_analysis(self, data):
+        if data:
+            return True
+        return False
---
+++ flask-app/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
+from Scraper import Scraper
+from Decision import Decision
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+scraper = Scraper()
+analyser = Decision()
+
+
+# We call scrape after the button has been clicked to check the review
+@app.route('/should-i-buy', methods=['GET'])
+@cross_origin()
+def check_product():
+    url = request.args.get('url')
+    data = scraper.scrape(url)
+
+    # call sentiment analysis on the data received
+    response = analyser.sentiment_analysis(data)
+
+    # send back a decision (buy or no buy)
+    if response:
+        return jsonify({"message": "this seems to be working!"})
+    return jsonify({"message": "il y a un problem"})
