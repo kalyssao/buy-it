105820264f12f8f9a49febbfcf003af94b586096
kalyssao <klysssa@icloud.com>
2020-10-15 13:32:34 +0000

    changes to app and css

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import Search from './Search';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App-page">
+        <h2 id="title">
+          Should You Buy that?
+        </h2>
+        <p id="subtitle">Let us be the judge of that.</p>
+        <Search></Search>
+      </div>
   );
 }
 
