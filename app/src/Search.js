551b3c740df46e655aecab592147d11c04264a0d
kalyssao <klysssa@icloud.com>
2020-10-16 22:12:04 +0000

    form confirmed, state passed up

--- app/src/Search.js
+++ app/src/Search.js
@@ -1,35 +1,21 @@
 import React, { useState } from 'react'
 import './misc.css'
 
-function Search() {
+const Search = ({ searchProduct }) => {
     const [query, setQuery] = useState('')
-    const [verdict, setVerdict] = useState('')
 
-    const searchProduct = async (e) => {
+    const handleSumbit = (e, query) => {
         e.preventDefault()
-        // some kind of loading indicator
-
-        const url = `http://127.0.0.1:5000/should-i-buy?url=${query}`
-
-        try {
-            const res = await fetch(url)
-            //setVerdict(res.)
-            console.log(res)
-        } catch (err) {
-            console.log(err)
-        }
-        
-        
     }
 
     return (
-        <div className="form">
+        <form onSubmit={handleSumbit} className="form">
             <input className="input" name="url" 
             placeholder="Enter product URL here"
             value={query}
             onChange={(e) => setQuery(e.target.value)}></input>
-            <button onClick={searchProduct} className="button" type="submit">JUDGE</button>
-        </div>
+            <button onClick={() => searchProduct(query)} className="button" type="submit">JUDGE</button>
+        </form>
     )
 }
 
