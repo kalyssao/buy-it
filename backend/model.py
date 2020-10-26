510bc922af09a297b9688f57c1a513e46f6143e1
kalyssao <klysssa@icloud.com>
2020-10-26 16:07:38 +0000

    init naive bayes model

---
+++ backend/model.py
@@ -0,0 +1,6 @@
+# i've decided to use naive bayes here.
+# in this file, i will preprocess and create a pipeline to run my inputs into
+# then i will split the data into train & test sets,
+# then figure out my metrics
+# after which i will save the model.
+# in decision.py i will load this model to make a prediction on a single item first.
