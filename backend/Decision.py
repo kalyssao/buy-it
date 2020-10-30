4cb39f9f86b55856dccde024b35c87bef25fde93
kalyssao <klysssa@icloud.com>
2020-10-30 11:50:34 +0000

    loading the model, preprocessing sample

--- backend/Decision.py
+++ backend/Decision.py
@@ -1,17 +1,48 @@
-# in here i will make a prediction after loading the saved naive bayes model
-# this will be done after doing the same pre-processing i did for the model on the json_input
-# i will work with only the first review then figure out the rest
-
+import pickle
+import numpy as np
+import pandas as pd
 
 class Decision:
-    # takes json file and pre-processes - returns data object
+    # takes json and pre-processes - returns data object
     def pre_processing(self, json_input):
-        print(json_input)
+        # place reviews in a data frame
+        df = pd.DataFrame(json_input['reviews'])
 
-    # loads the saved model and calls it on the data object
-    def sentiment_analysis(self, data):
-        if data:
-            # this is where I load the model
-            # this is where i call model.fit model.predict(data)
-        else:
+        # set to lowercase & strip punctuation
+        df['content'] = df.content.map(lambda x: x.lower())
+        df['content'] = df.content.str.replace(r"[^\w\s]",'')
+        
+        return df['content']
+
+    # takes the array of labels and returns the prevalent response
+    def make_decision(self, labels):
+
+        (unique, counts) = np.unique(labels, return_counts=True)
+        frequencies = np.asarray((unique, counts)).T
+
+        negative = frequencies[0][1]
+        positive = frequencies[1][1]
+
+        if negative > positive:
             return False
+        
+        return True
+
+    # does pre-processing, loads the saved model and calls it on the data
+    def sentiment_analysis(self, data):
+        processed = self.pre_processing(data)
+
+        if processed is not None:
+            print("Preprocessed \n", processed)
+
+            # load model with pickle
+            with open("amazon_naive_bayes.pkl", 'rb') as file:
+                pickled_model = pickle.load(file)
+            
+            sentiment_labels = pickled_model.predict(processed)
+            decision = self.make_decision(sentiment_labels)
+
+            return decision
+
+        else:
+            return None
