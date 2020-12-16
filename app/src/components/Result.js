b727859a7d60c3a05a143256c799942b5f5e91f9
kalyssao <klysssa@icloud.com>
2020-12-16 20:38:23 +0000

    getting verdict info! (:

--- app/src/components/Result.js
+++ app/src/components/Result.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
 
-export default function Result({ verdict }) {
+export default function Result() {
+    const location = useLocation()
+    const [ verdict, setVerdict ] = useState()
+
+    useEffect(() => {
+        setVerdict(location.decision)
+    }, [location])
     return (
         <div>
+            Hello World!
             { verdict !== null
                 ? 
                 <div className="positive--result">
                     {/* {product.product_title}
                     {product.next_page}
                     {product.image}
-                    {product.reviews} */}
-                    buy it.
+                    {product.reviews}
+                    buy it. */}
+                    Buy it
                 </div>
 
                 : <div className="negative--result">
